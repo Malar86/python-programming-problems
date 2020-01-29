1a3a08dcc57aff9e0d3928a759407750bafa9e69
Malar86 <60420195+Malar86@users.noreply.github.com>
2020-01-29 15:32:19 +0530

    Update pg7.py

--- pg7.py
+++ pg7.py
@@ -10,20 +10,13 @@ n=8
 while n>0:
     ip=int(input("enter a number"))
     b=random.randrange(0,3)
-    if(ip==0 and b==0):
+    if(ip==0 and b==0 or ip==2 and b==2 or ip==1 and b==1):
         print("try again")
-    elif(ip==1 and b==1):
-        print("try again")
-    elif(ip==2 and b==2):
-        print("try again")
-    elif(ip==0 and b==1 or ip==1 and b==2):
+    elif(ip==0 and b==1 or ip==1 and b==2 or ip==2 and b==0):
         print("you lost")
-    elif(ip==2 and b==1):
+    elif(ip==1 and b==0 or ip==2 and b==0 or ip==0 and b==2):
         print("you won")
-    elif(ip==2 and b==0):
-        print("you lost")
-    elif(ip==1 and b==0):
-        print("you won")
-    elif(ip==0 and b==2):
-        print("you won")
+        break
+    
+
     
