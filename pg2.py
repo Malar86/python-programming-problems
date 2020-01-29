7fa7ed43b69c2107c0d1170316b8052669abe143
Malar86 <60420195+Malar86@users.noreply.github.com>
2020-01-29 11:34:34 +0530

    Uber billing
    Uber billing

---
+++ pg2.py
@@ -0,0 +1,10 @@
+def myfun(x,y):
+   if(x=='a' and y=='b'):
+       print("the bill amt is 100")
+   elif(x=='a' and y=='c'):
+       print("the bill amt is 300")
+   elif(x=='b' and x=='c'):
+        print("the bill amt is 150")
+   
+
+n=myfun('a','c')
